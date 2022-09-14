361da8829ac616a82c8f80923548a80f177e08f8
VladimirFedoseev <fedoseev885@yandex.ru>
2022-09-14 18:53:49 +0700

    1409

--- school/models.py
+++ school/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import User
 
 
 class SSlider(models.Model):
@@ -12,3 +13,18 @@ class SSlider(models.Model):
     class Meta:
         verbose_name = 'Слайд'
         verbose_name_plural = 'Слайды'
+
+
+class AllCourses(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название курса')
+    image = models.ImageField(upload_to='school_images', blank=True, verbose_name='Изображение')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    short_description = models.CharField(max_length=100, blank=True, verbose_name='Короткое описание')
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0, verbose_name='Цена')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
--- school_page/urls.py
+++ school_page/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from school import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.first_page),
     path('index/', views.first_page, name='index'),
+    path('users/', include('users.urls')),
 ]
 
 if settings.DEBUG:
